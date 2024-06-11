8f3e902246f4c0666372bfd2d42b6b6c58de3b0b
muhammad zaky <muhammad.zaky.firdaus@mail.ugm.ac.id>
2024-06-11 08:23:00 +0700

    edit

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ model_path = os.path.join(os.getcwd(), "aktivio9986")
 cnn_model = tf.keras.models.load_model(model_path)
 
 # Set API Key
-# GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
-os.environ["GOOGLE_API_KEY"] = "AIzaSyBSlPzxVot-SwXlFevsUAnq53S58yjojUM"
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+# os.environ["GOOGLE_API_KEY"] = "AIzaSyBSlPzxVot-SwXlFevsUAnq53S58yjojUM"
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 @app.route('/api/classify', methods=['POST'])
