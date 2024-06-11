14e4cd49c557f571cc5b818ebdfd83c608c29784
muhammad zaky <muhammad.zaky.firdaus@mail.ugm.ac.id>
2024-06-11 08:30:10 +0700

    aaa

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def predict():
     return jsonify({"class_label": class_label, "nutrition_response": response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
