55e75e45216e7c03b185adf5c1df48fe1a9dec0e
muhammad zaky <muhammad.zaky.firdaus@mail.ugm.ac.id>
2024-06-12 11:03:02 +0700

    prompt edit

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -26,6 +26,20 @@ def prep_image(uploaded_file):
     else:
         raise FileNotFoundError("No File is uploaded!")
 
+# def prep_image(uploaded_file):
+#     if uploaded_file is not None:
+#         # Read the file as bytes
+#         bytes_data = uploaded_file.getvalue()
+#         image_parts = [
+#             {
+#                 "mime_type": uploaded_file.type,
+#                 "data": bytes_data
+#             }
+#         ]
+#         return image_parts
+#     else:
+#         raise FileNotFoundError("No File is uploaded!")
+
 def prep_image_cnn(image):
     image = image.resize((224, 224))
     image_array = np.array(image) / 255.0
