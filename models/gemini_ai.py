70139f554cea033202f7fe3d81582d81d4f25a44
muhammad zaky <muhammad.zaky.firdaus@mail.ugm.ac.id>
2024-06-12 19:12:08 +0700

    Update gemini_ai.py

--- models/gemini_ai.py
+++ models/gemini_ai.py
@@ -6,12 +6,18 @@ def get_response_nutrition(image_data, prompt):
     try:
         model = genai.GenerativeModel('gemini-pro-vision')
         response = model.generate_content([image_data[0], prompt])
+        
         # Clean the response string
         cleaned_response = re.sub(r'\n|\s+', ' ', response.text).strip()
 
         # Parse the cleaned response into a JSON object
         nutrition_data = json.loads(cleaned_response)
+
+        # Rename the "Ukuran porsi" property to "porsi"
+        if "Ukuran porsi" in nutrition_data:
+            nutrition_data["porsi"] = nutrition_data.pop("Ukuran porsi")
+
         return nutrition_data
     except Exception as e:
-        print(f"Error during API call: {e}")
+        print(f"Error during API call or JSON parsing: {e}")
         return None
